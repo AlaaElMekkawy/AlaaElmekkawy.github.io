1cd4c237acd4c9000383dbb30d34ce966d0ccc44
AlaaElMekkawy <eng_alaa2004@yahoo.com>
2017-01-27 19:52:00 -0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,29 @@
+/*global $*/
+
+
+$(function () {
+    'use strict';
+    
+    
+    /*
+    start swiper
+    */
+    var swiper = new Swiper('.swiper-container', {
+        pagination: '.swiper-pagination',
+        paginationClickable: true,
+        nextButton: '.swiper-button-next',
+        prevButton: '.swiper-button-prev',
+        spaceBetween: 30,
+        effect: 'fade',
+        autoplay: 4000
+
+    });
+    
+    
+
+    
+    
+
+});
+
+
